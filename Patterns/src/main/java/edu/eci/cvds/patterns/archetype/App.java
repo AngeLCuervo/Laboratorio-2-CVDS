8de1fb7ab09d2fa897fe0de953da4c95ad5111b3
AngeLCuervo <angel.cuervo@mail.escuelaing.edu.co>
2025-02-04 22:03:51 -0500

    lab2 finalizado

--- Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        if (args.length > 0) {
+            System.out.println( "Bienvenidos " + String.join(" ", args));
+        }
+        else {
+            System.out.println( "Hello World!" );
+        }
+           
     }
 }
